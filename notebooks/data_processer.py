fe5a31c528808fe46d341c9eb66caf377a151233
P1N60 <victor.guldregn@gmail.com>
2025-06-28 10:01:02 +0200

    added method for key financials

--- notebooks/data_processer.py
+++ notebooks/data_processer.py
@@ -16,14 +16,45 @@ class Stock:
     
     def get_data_key(self):
         key_financials = self.get_key_financials()
-        latest_earn_date = key_financials.keys().to_list()[0]
-        df = pd.DataFrame(index=[0])
-        df["Ticker"] = self.symbol
-        df["Name"] = self.name
-        df["Date"] = latest_earn_date
-        df["3M Future Change"], df["6M Future Change"], df["9M Future Change"], df["1Y Future Change"] = np.nan, np.nan, np.nan, np.nan
-        for feature in key_financials.index.to_list():
-            df[feature] = key_financials[latest_earn_date][feature]
+        earn_dates = key_financials.keys().to_list()
+        df = pd.DataFrame()
+        for earn_date in earn_dates:
+            row_df = pd.DataFrame(index=[0])
+            row_df["Ticker"] = self.symbol
+            row_df["Name"] = self.name
+            row_df["Date"] = earn_date
+            if earn_date == earn_dates[0]:
+                row_df["3M Future Change"], df["6M Future Change"], df["9M Future Change"], df["1Y Future Change"] = np.nan, np.nan, np.nan, np.nan
+            else:
+                price_data = yf.download(self.symbol, period="max", rounding=False, progress=False)
+                got_price = False
+                day_offset = 0
+                while(got_price==False and day_offset > -6):
+                    try:           
+                        row_df['3M Future Change'] = (
+                        price_data.loc[pd.Timestamp(earn_date) + pd.Timedelta(days=day_offset, weeks=13), ('Close', self.symbol)] / 
+                        price_data.loc[pd.Timestamp(earn_date) + pd.Timedelta(days=day_offset), ('Close', self.symbol)] - 1
+                        )
+                        row_df['6M Future Change'] = (
+                            price_data.loc[pd.Timestamp(earn_date) + pd.Timedelta(days=day_offset, weeks=26), ('Close', self.symbol)] / 
+                            price_data.loc[pd.Timestamp(earn_date) + pd.Timedelta(days=day_offset), ('Close', self.symbol)] - 1
+                        )
+                        row_df['9M Future Change'] = (
+                            price_data.loc[pd.Timestamp(earn_date) + pd.Timedelta(days=day_offset, weeks=39), ('Close', self.symbol)] / 
+                            price_data.loc[pd.Timestamp(earn_date) + pd.Timedelta(days=day_offset), ('Close', self.symbol)] - 1
+                        )
+                        row_df['1Y Future Change'] = (
+                            price_data.loc[pd.Timestamp(earn_date) + pd.Timedelta(days=day_offset, weeks=52), ('Close', self.symbol)] / 
+                            price_data.loc[pd.Timestamp(earn_date) + pd.Timedelta(days=day_offset), ('Close', self.symbol)] - 1
+                        )
+                        got_price = True
+                    except:
+                        day_offset += -1
+                if pd.isna(row_df['3M Future Change']) or pd.isna(row_df['3M Future Change']) or pd.isna(row_df['3M Future Change']) or pd.isna(row_df['3M Future Change']):
+                    continue
+            for feature in key_financials.index.to_list():
+                row_df[feature] = key_financials[earn_date][feature]
+            df = pd.concat([df, row_df])
         return df
 
 # # a must-have
